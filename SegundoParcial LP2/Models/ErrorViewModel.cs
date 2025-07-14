53b5205b380f63a8d2abdba141ff544b4300284f
JuanCarrion29 <jc21-1064@unphu.edu.do>
2025-07-14 11:01:49 -0400

    Add project files.

---
+++ SegundoParcial LP2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SegundoParcial_LP2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
